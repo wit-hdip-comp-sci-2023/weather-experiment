c2fce88a27b0ff79b50957f066c7e095ce515eae
Eamonn de Leastar <edeleastar@wit.ie>
2023-06-16 12:39:58 +0100

    extract trend data

--- controllers/dashboard-controller.js
+++ controllers/dashboard-controller.js
@@ -15,7 +15,7 @@ export const dashboardController = {
     const lat = request.body.lat;
     const lng = request.body.lng;
     const requestUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lng}&units=metric&appid=YOUR-API-KEY`
-    const result = await axios.get(oneCallRequest);
+    const result = await axios.get(requestUrl);
     if (result.status == 200) {
       const reading = result.data.current;
       report.code = reading.weather[0].id;
@@ -23,6 +23,15 @@ export const dashboardController = {
       report.windSpeed = reading.wind_speed;
       report.pressure = reading.pressure;
       report.windDirection = reading.wind_deg;
+
+      report.tempTrend = [];
+      report.trendLabels = [];
+      const trends = result.data.daily;
+      for (let i=0; i<trends.length; i++) {
+        report.tempTrend.push(trends[i].temp.day);
+        const date = new Date(trends[i].dt * 1000);
+        report.trendLabels.push(`${date.getDate()}/${date.getMonth()}/${date.getFullYear()}` );
+      }
     }
     console.log(report);
     const viewData = {
